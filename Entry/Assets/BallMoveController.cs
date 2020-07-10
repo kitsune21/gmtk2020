a9a1949f86d61f53f93b2c82fbe05833196f0ed1
Mark <mwhittaker00@gmail.com>
2020-07-10 16:33:59 -0600

    Hit ball and bounce off of walls

---
+++ Entry/Assets/BallMoveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float maxForce;
+    public float actualForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // testing hitting from an angle, delete when we get player provided direction
+        transform.rotation = Quaternion.Euler(0, 0, 170);
+        HitBall(transform.right, actualForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HitBall(Vector2 dir, float power)
+    {
+        float force = maxForce * power;
+        rb.AddForce(dir * force, ForceMode2D.Impulse);
+    }
+}
