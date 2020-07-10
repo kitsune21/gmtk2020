a7f7adb7aa15aaf10be3669a2d216c7538487932
kitsune21 <kitsunesama21@gmail.com>
2020-07-10 17:30:17 -0600

    Merging stuff
    plz work

--- Entry/Assets/BallMoveController.cs
+++ Entry/Assets/BallMoveController.cs
@@ -7,15 +7,15 @@ public class BallMoveController : MonoBehaviour
 
     private Rigidbody2D rb;
     public float maxForce;
-    public float actualForce;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         // testing hitting from an angle, delete when we get player provided direction
-        transform.rotation = Quaternion.Euler(0, 0, 170);
-        HitBall(transform.right, actualForce);
+        //transform.rotation = Quaternion.Euler(0, 0, 170);
+        //HitBall(transform.right, actualForce);
+
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class BallMoveController : MonoBehaviour
         
     }
 
-    void HitBall(Vector2 dir, float power)
+    public void HitBall(Vector3 dir, float power)
     {
         float force = maxForce * power;
         rb.AddForce(dir * force, ForceMode2D.Impulse);
---
+++ Entry/Assets/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerRotateAmount;
+
+    public float increasePowerMeterAmount;
+    private int powerMeterDirection = 1;
+
+    public Slider powerMeter;
+
+    public bool startPowerMeter;
+
+    private float powerMeterFinalValue;
+
+    public GameObject myBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPowerMeter)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, 0, playerRotateAmount);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, 0, -playerRotateAmount);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (startPowerMeter)
+            {
+                stopPowerMeter();
+            }
+            else
+            {
+                startPowerMeter = true;
+            }
+        }
+        updatePowerMeter();
+    }
+
+    private void updatePowerMeter()
+    {
+        if (startPowerMeter)
+        {
+            powerMeter.value += ((float)powerMeterDirection * increasePowerMeterAmount);
+            if (powerMeter.value >= 1)
+            {
+                powerMeterDirection = -1;
+            }
+            else if (powerMeter.value <= 0)
+            {
+                powerMeterDirection = 1;
+            }
+        }
+    }
+
+    private void stopPowerMeter()
+    {
+        startPowerMeter = false;
+        powerMeterFinalValue = powerMeter.value;
+        hitTheBall();
+    }
+
+    private void hitTheBall()
+    {
+        myBall.GetComponent<BallMoveController>().HitBall(getRotation(), getPowerMeterValue());
+    }
+
+    private void resetCharacter()
+    {
+        powerMeterDirection = 1;
+        startPowerMeter = false;
+        powerMeterFinalValue = 0;
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    public float getPowerMeterValue()
+    {
+        return powerMeterFinalValue;
+    }
+
+    public Vector3 getRotation()
+    {
+        return transform.up;
+    }
+}
