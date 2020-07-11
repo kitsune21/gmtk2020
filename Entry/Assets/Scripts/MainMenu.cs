2390e2fde77e7970e8d95fac6a82a22033dcb094
Mark <mwhittaker00@gmail.com>
2020-07-10 18:45:10 -0600

    Added title screen

---
+++ Entry/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string newScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
