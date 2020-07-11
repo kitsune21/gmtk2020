98e194e67fa43691f2f7766161728ca53e1e88e8
Mark <mwhittaker00@gmail.com>
2020-07-11 00:03:50 -0600

    Change levels, player stats persist

--- Entry/Assets/Scripts/HitGoal.cs
+++ Entry/Assets/Scripts/HitGoal.cs
@@ -9,20 +9,39 @@ public class HitGoal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // find the starting point
+        foreach (GameObject go in GameObject.FindObjectsOfType(typeof(GameObject)))
+        {
+            Debug.Log(go.tag);
+            if (go.CompareTag("Respawn"))
+            {
+                foreach (GameObject pl in GameObject.FindObjectsOfType(typeof(GameObject)))
+                {
+                    if (pl.CompareTag("Ball"))
+                    {
+                        // transport player landing's position
+                        pl.gameObject.transform.position = go.transform.position;
+                        // stop the ball 
+                        pl.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-
+        
     private void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log(col.gameObject.tag);
         if (col.gameObject.tag == "Ball")
         {
+            // this holds the canvas and other objects
+            GameObject parentContainer = col.gameObject.transform.parent.gameObject.transform.parent.gameObject;
+            DontDestroyOnLoad(parentContainer);
             SceneManager.LoadScene(nextScene);
         }
     }
