72d16850dc764a3937f3b5e11b1e2993123e067d
kitsune21 <kitsunesama21@gmail.com>
2020-07-11 12:14:53 -0600

    Club lifes
    Added in the club life ui
    will reset back to 0 rage when dead

---
+++ Entry/Assets/Scripts/ClubController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClubController : MonoBehaviour
+{
+    public int clubCount;
+    public int clubCountMax;
+
+    public GameObject clubLife;
+    private List<GameObject> clubLifeList;
+
+    void Start()
+    {
+        clubLifeList = new List<GameObject>();
+        for (int i = clubCountMax; i > 0; i--)
+        {
+            GameObject newLife = Instantiate(clubLife, transform);
+            newLife.GetComponent<LifeController>().setId(i);
+            clubLifeList.Add(newLife);
+        }
+        clubCount = clubCountMax;
+    }
+
+    public int playerDied()
+    {
+        if (clubCount > 0)
+        {
+            int nextID = (clubCountMax + 1) - clubCount;
+            foreach (GameObject life in clubLifeList)
+            {
+                if (life.GetComponent<LifeController>().getId() == nextID)
+                {
+                    life.GetComponent<LifeController>().setIsGone();
+                }
+            }
+        }
+        return clubCount;
+    }
+}
---
+++ Entry/Assets/Scripts/LifeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeController : MonoBehaviour
+{
+    public int id;
+
+    private bool isGone;
+   
+
+    public void setIsGone()
+    {
+        isGone = true;
+        Image myImage = gameObject.GetComponentInChildren<Image>();
+        myImage.color = new Color(myImage.color.r, myImage.color.b, myImage.color.g, 0.3f);
+    }
+
+    public bool getIsGone()
+    {
+        return isGone;
+    }
+
+    public void setId(int newID)
+    {
+        id = newID;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
